4dc2d08c862b3138673053f57af0a9e20f510774
MwSpace llc <app@mwspace.com>
2023-04-24 05:22:55 +0200

    + aka.providers

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {app} from "./providers/express.js";
 import config from "./config/app.js";
+import {app} from "aka.providers";
 
 /*
  |--------------------------------------------------------------------------
