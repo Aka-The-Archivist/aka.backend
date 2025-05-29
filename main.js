40b5b4c90d04293569322e283be32da65adce365
mwspace.com <app@mwspace.com>
2025-05-29 14:50:54 +0200

    update 1.0.1

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-import { app } from "aka.providers"; // must init before all
+import {app, ip} from "aka.providers"; // must init before all
 import config from "./config/app.js";
 
-/* 
+/*
  |--------------------------------------------------------------------------
  | Start [aka.backend] application
  |--------------------------------------------------------------------------
  */
 app.listen(config.port, () => {
-  console.log(
-    `${config.name} backend listening on port http://127.0.0.1:${config.port}`
-  );
-});
+  console.log(`${config.name} backend listening on local port http://${ip.local}:${config.port}`)
+  console.log(`${config.name} backend listening on network port http://${ip.network}:${config.port}`)
+})
 
 /*
  |--------------------------------------------------------------------------
